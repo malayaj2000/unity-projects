1e09ee3744abfab8114f3371b6273e5d7ef2b86b
malayaj2000 <65330090+malayaj2000@users.noreply.github.com>
2020-07-28 18:47:58 +0530

    cube motion

---
+++ New Unity Project (3)/Assets/motion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class motion : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0.2f, 0);
+        }
+        gameObject.transform.Translate(direction.normalized * Time.deltaTime * speed);
+        
+    }
+}
